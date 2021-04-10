de9191168df0e00e602643dec842987b63315f5e
James Cahyadi <jcahyadi239@gmail.com>
2021-04-10 16:07:43 -0400

    fix ga helper

--- src/utils/google-analytics-helpers.js
+++ src/utils/google-analytics-helpers.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 
 export const GAevent = (categoryName, eventName, labelName) => {
-  ReactGA.event({
+  return ReactGA.event({
     category: categoryName,
     action: eventName,
     label: labelName,
