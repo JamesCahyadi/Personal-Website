1fee84105779627fab96c8332b6b8b285936f9de
James Cahyadi <jcahyadi239@gmail.com>
2021-10-18 23:27:13 -0400

    update course component

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import { makeStyles } from '@material-ui/core/styles';
+import Avatar from "@material-ui/core/Avatar";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import React from "react";
+import { getSubjectIcon } from "~/utils/getSubjectIcon";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-    root: {
-        padding: 1
-    },
-    text: {
-        fontSize: 1
-    }
+  root: {
+    padding: 1,
+  },
+  text: {
+    fontSize: 1,
+  },
 });
 
-const Course = ({ course, icon }) => {
-    const classes = useStyles();
+const Course = ({ course, subject }) => {
+  const classes = useStyles();
 
-    return (
-        <ListItem className={classes.root}>
-            <ListItemAvatar>
-                <Avatar>
-                    {icon}
-                </Avatar>
-            </ListItemAvatar>
-            <ListItemText className={classes.text} secondary={course} />
-        </ListItem>
-    );
-}
+  return (
+    <ListItem className={classes.root}>
+      <ListItemAvatar>
+        <Avatar>{getSubjectIcon(subject)}</Avatar>
+      </ListItemAvatar>
+      <ListItemText className={classes.text} secondary={course} />
+    </ListItem>
+  );
+};
 
 export default Course;
